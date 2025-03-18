071bf8161c54f22f7fe7e2d7c46b26ef412f22e5
neru <ngocngocthuc@gmail.com>
2025-03-18 23:19:16 +0700

    user repository

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SBS.Repositories.Base;
+using SBS.Repositories.Models;
+
+namespace SBS.Repositories
+{
+    public class UserRepository : GenericRepository<User>
+    {
+        public UserRepository() : base()
+        {
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+            => await DbSet.Include(item => item.Transactions).ToListAsync();
+
+        public async Task<User?> GetUserAsync(Guid id)
+            => await DbSet.Include(item => item.Transactions).FirstOrDefaultAsync(item => item.Id == id);
+
+        public async Task<List<User>> Search(string? username, string? email, string? status)
+            => await DbSet.Include(item => item.Transactions)
+                .Where(item =>
+                    (string.IsNullOrEmpty(username) || item.Username.Contains(username)) &&
+                    (string.IsNullOrEmpty(email) || item.Email.Contains(email)) &&
+                    (string.IsNullOrEmpty(status) || item.Status == status)
+                )
+                .ToListAsync();
+    }
+}
