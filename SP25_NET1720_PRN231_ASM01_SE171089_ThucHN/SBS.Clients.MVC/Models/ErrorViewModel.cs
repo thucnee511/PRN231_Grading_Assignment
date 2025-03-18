f10698e50ce7e675b3e9f0749d59f435329d0a9e
neru <ngocngocthuc@gmail.com>
2025-03-18 20:02:58 +0700

    create asm 01 solution

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Clients.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SBS.Clients.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
