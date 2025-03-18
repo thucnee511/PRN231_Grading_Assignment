384d5cc8bb321a99ab8f84c8c282d0dbd121dc9b
neru <ngocngocthuc@gmail.com>
2025-03-18 23:29:38 +0700

    transaction service

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Services/TransactionService.cs
@@ -0,0 +1,33 @@
+﻿using SBS.Repositories;
+using SBS.Repositories.Models;
+
+namespace SBS.Services
+{
+    public interface ITransactionService
+    {
+        Task<List<Transaction>> GetTransactionsAsync();
+        Task<Transaction?> GetTransactionAsync(Guid id);
+        Task<int> AddAsync(Transaction transaction);
+        Task<int> UpdateAsync(Transaction transaction);
+        Task<int> DeleteAsync(Transaction transaction);
+    }
+    public class TransactionService : ITransactionService
+    {
+        private readonly TransactionRepository _transactionRepository;
+        public TransactionService()
+        {
+            _transactionRepository = new();
+        }
+
+        public async Task<List<Transaction>> GetTransactionsAsync()
+            => await _transactionRepository.GetAllAsync();
+        public async Task<Transaction?> GetTransactionAsync(Guid id)
+            => await _transactionRepository.GetOneAsync(id);
+        public async Task<int> AddAsync(Transaction transaction)
+            => await _transactionRepository.AddAsync(transaction);
+        public async Task<int> UpdateAsync(Transaction transaction)
+            => await _transactionRepository.UpdateAsync(transaction);
+        public async Task<int> DeleteAsync(Transaction transaction)
+            => await _transactionRepository.DeleteAsync(transaction);
+    }
+}
