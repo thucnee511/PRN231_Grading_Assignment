d1b14e2f9f92752fec6a09522be4be13b6edb312
neru <ngocngocthuc@gmail.com>
2025-03-18 23:16:10 +0700

    user account repository

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Repositories/UserAccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SBS.Repositories.Base;
+using SBS.Repositories.Models;
+
+namespace SBS.Repositories
+{
+    public class UserAccountRepository : GenericRepository<UserAccount>
+    {
+        public UserAccountRepository() : base()
+        {
+        }
+
+        public async Task<UserAccount?> Login(string username, string password)
+            => await DbSet.FirstOrDefaultAsync(item => item.UserName == username && item.Password == password && item.IsActive);
+    }
+}
