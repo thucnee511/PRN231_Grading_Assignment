f975ea39881cfdc792fff744e4b6b8fd0b1a5c4f
neru <ngocngocthuc@gmail.com>
2025-03-18 23:20:10 +0700

    transaction repository

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Repositories/TransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using SBS.Repositories.Base;
+using SBS.Repositories.Models;
+
+namespace SBS.Repositories
+{
+    public class TransactionRepository : GenericRepository<Transaction>
+    {
+        public TransactionRepository() : base()
+        {
+        }
+    }
+}
