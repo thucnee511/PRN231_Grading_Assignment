9e5363d3b9eae33232e10871f548e58a07fd268d
neru <ngocngocthuc@gmail.com>
2025-03-18 23:26:56 +0700

    user service

---
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using SBS.Repositories;
+using SBS.Repositories.Models;
+
+namespace SBS.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllUsersAsync();
+        Task<User?> GetUserAsync(Guid id);
+        Task<List<User>> SearchUsers(string? username, string? email, string? status);
+        Task<int> AddAsync(User user);
+        Task<int> UpdateAsync(User user);
+        Task<int> DeleteAsync(User id);
+    }
+    public class UserService : IUserService
+    {
+        private readonly UserRepository _userRepository;
+        public UserService()
+        {
+            _userRepository = new();
+        }
+        public async Task<List<User>> GetAllUsersAsync()
+            => await _userRepository.GetUsersAsync();
+        public async Task<User?> GetUserAsync(Guid id)
+            => await _userRepository.GetUserAsync(id);
+        public async Task<List<User>> SearchUsers(string? username, string? email, string? status)
+            => await _userRepository.Search(username, email, status);
+        public async Task<int> AddAsync(User user)
+            => await _userRepository.AddAsync(user);
+        public async Task<int> UpdateAsync(User user)
+            => await _userRepository.UpdateAsync(user);
+        public async Task<int> DeleteAsync(User user)
+            => await _userRepository.DeleteAsync(user);
+    }
+}
