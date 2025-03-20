4d6401b84c43a4be0216542ec47d0c53ebed1d62
neru <ngocngocthuc@gmail.com>
2025-03-20 11:21:18 +0700

    configure ApiServices

--- SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.APIServices/Program.cs
+++ SP25_NET1720_PRN231_ASM01_SE171089_ThucHN/SBS.APIServices/Program.cs
@@ -1,4 +1,15 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OData;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OpenApi.Models;
+using SBS.Repositories.Models;
+using SBS.Services;
+using System.Text;
+using System.Text.Json.Serialization;
+
 namespace SBS.APIServices
 {
     public class Program
@@ -6,14 +17,71 @@ namespace SBS.APIServices
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddScoped<IUserAccountService, UserAccountService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<ITransactionService, TransactionService>();
             // Add services to the container.
-
-            builder.Services.AddControllers();
+            static IEdmModel GetEdmModel()
+            {
+                var odataBuilder = new ODataConventionModelBuilder();
+                odataBuilder.EntitySet<User>("User");
+                odataBuilder.EntitySet<Transaction>("Transsaction");
+                return odataBuilder.GetEdmModel();
+            }
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+            }).AddOData(options =>
+            {
+                options.Select().Filter().OrderBy().Expand().SetMaxTop(null).Count();
+                options.AddRouteComponents("odata", GetEdmModel());
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.ResolveConflictingActions(ApiDes => ApiDes.First());
+                option.DescribeAllParametersInCamelCase();
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            //Configure Authen via Jwt
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,7 +90,10 @@ namespace SBS.APIServices
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            // Configure Cors
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            //Configure OData
+            
             app.UseAuthorization();
 
 
